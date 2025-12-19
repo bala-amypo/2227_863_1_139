8399269f91ecb61435444a1bb0abaec34792d025
bala-amypo <bala@amypo.in>
2025-12-19 02:08:52 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -23,5 +23,22 @@ public class User{
 
     @Ennumerated(EnumType.String)
     @Column(nullable = false)
+    private Role role;
+
+    public Long getId(){
+        return id;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
     
 }
