e0e62254be73077358c18afff35202c34e751905
bala-amypo <bala@amypo.in>
2025-12-19 08:16:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -11,16 +11,14 @@ package com.example.demo.entity;
 @Table(name = "universities")
 public class University{
         
-
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
         
         @Column(nullable=false,unique=true)
         private String name;
-
+        
+        @Column(nullable=false)
         private String country;
-
-
-
+        
 }
