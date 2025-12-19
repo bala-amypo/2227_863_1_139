514ef9d49619e5e15cd2604215400c5036ab6810
bala-amypo <bala@amypo.in>
2025-12-19 09:16:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/Course.java
+++ src/main/java/com/example/demo/entity/Course.java
@@ -6,6 +6,15 @@ package com.example.demo.entity;
 public class Course{
      
     @Id
-
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable=false,unique=true)
+    private String code;
+
+    @Column(nullable=false)
+    private String Title;
+
+    @Column(nullable=false)
+    private String credits;
 }
