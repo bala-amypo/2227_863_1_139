6951c082ca0ffeb591679e017b2ddd01eeaffa07
bala-amypo <bala@amypo.in>
2025-12-21 04:48:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferRequest.java
+++ src/main/java/com/example/demo/entity/TransferRequest.java
@@ -1,43 +1,90 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "transfer_requests")
-public class TransferRequest{
+public class TransferRequest {
 
-   @Id
-   @GeneratedValue(strategy=GenerationType.IDENTITY)
-   private Long id;   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-   private String studentId;
+    private String studentId;
 
-   @ManyToOne
-   @JoinColumn(name="source_program_id")
-   private Program sourceProgram;
+    @ManyToOne
+    @JoinColumn(name = "source_program_id")
+    private Program sourceProgram;
 
-   @ManyToOne
-   @JoinColumn(name="target_program_id")
-   private Program targetProgram;
-   
-   @OneToMany(cascade = CascadeType.ALL)
-   private List<CompletedCourse> completedcourses;
+    @ManyToOne
+    @JoinColumn(name = "target_program_id")
+    private Program targetProgram;
 
-   @Enumerated(EnumType.STRING)
-   private TransferStatus status;
-   
-   public TransferRequest(String studentId,Program sourceProgram,Program targetProgram,List<CompletedCourse> completedcourses,TransferStatus status ){
-      this.studentId=studentId;
-      this.sourceProgram=sourceProgram;
-      this.targetProgram=targetProgram;
-      this.completedcourses=completedcourses;
-      this.status=status;
-   }
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<CompletedCourse> completedCourses;
+
+    @Enumerated(EnumType.STRING)
+    private TransferStatus status;
+
+    public TransferRequest() {
+    }
+
+    public TransferRequest(
+            String studentId,
+            Program sourceProgram,
+            Program targetProgram,
+            List<CompletedCourse> completedCourses,
+            TransferStatus status) {
+
+        this.studentId = studentId;
+        this.sourceProgram = sourceProgram;
+        this.targetProgram = targetProgram;
+        this.completedCourses = completedCourses;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public Program getSourceProgram() {
+        return sourceProgram;
+    }
+
+    public void setSourceProgram(Program sourceProgram) {
+        this.sourceProgram = sourceProgram;
+    }
+
+    public Program getTargetProgram() {
+        return targetProgram;
+    }
+
+    public void setTargetProgram(Program targetProgram) {
+        this.targetProgram = targetProgram;
+    }
+
+    public List<CompletedCourse> getCompletedCourses() {
+        return completedCourses;
+    }
+
+    public void setCompletedCourses(List<CompletedCourse> completedCourses) {
+        this.completedCourses = completedCourses;
+    }
+
+    public TransferStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransferStatus status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/example/demo/entity/University.java
+++ src/main/java/com/example/demo/entity/University.java
@@ -1,30 +1,45 @@
 package com.example.demo.entity;
 
-
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "universities")
-public class University{
-        
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        
-        @Column(unique=true)
-        private String name;
-        
-        private String country;
-        
+public class University {
 
-        public University(String name,String country){
-                this.name=name;
-                this.country=country;
-        }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String country;
+
+    public University() {
+    }
+
+    public University(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
