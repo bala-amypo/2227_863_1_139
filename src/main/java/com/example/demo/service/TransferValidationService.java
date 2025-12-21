7274ef2624c171b9ca36b37b119b7bb13be68681
bala-amypo <bala@amypo.in>
2025-12-21 09:09:19 +0000

    User commit

--- src/main/java/com/example/demo/service/TransferValidationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.TransferEvaluationResult;
-import com.example.demo.entity.TransferRequest;
-
-public interface TransferValidationService {
-
-    TransferEvaluationResult evaluateTransfer(TransferRequest request);
-}
