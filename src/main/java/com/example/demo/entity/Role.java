f51d2ff3deb6651ce4d0535991bc17c4f87d6103
bala-amypo <bala@amypo.in>
2025-12-26 05:36:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,25 +1,7 @@
-import com.example.demo.entity.Role;
+package com.example.demo.entity;
 
-@Override
-public String register(RegisterRequest request) {
-
-    Role role;
-    try {
-        role = Role.valueOf(request.getRole().toUpperCase());
-    } catch (Exception e) {
-        throw new RuntimeException("Invalid role");
-    }
-
-    if (userRepo.findByEmail(request.getEmail()).isPresent()) {
-        throw new RuntimeException("Email already registered");
-    }
-
-    User user = new User();
-    user.setEmail(request.getEmail());
-    user.setPassword(encoder.encode(request.getPassword()));
-    user.setRoles(Set.of("ROLE_" + role.name()));
-
-    userRepo.save(user);
-
-    return "User registered successfully";
+public enum Role {
+    ADMIN,
+    STUDENT,
+    EVALUATOR
 }
